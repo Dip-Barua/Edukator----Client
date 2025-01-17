4672758329f9dfc430f92c9afbe5dff65edb291d
Dip <65559847+Dip-Barua@users.noreply.github.com>
2025-01-17 14:31:46 +0600

    Added :  Headlines component for structured headings for each section

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../../Coverages/Banner';
+import Partners from '../../Coverages/Partners';
+import Becomeinstructor from '../../Coverages/Becomeinstructor';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>    
+            <Partners></Partners>    
+            <Becomeinstructor></Becomeinstructor>    
+        </div>
+    );
+};
+
+export default Home;
