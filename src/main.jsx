0fe0fc91bf9d1a2cbbee61678ded11d519c2ab9e
Dip <65559847+Dip-Barua@users.noreply.github.com>
2025-01-18 13:44:44 +0600

    Added : Integrated AuthProvider and QueryClientProvider in main.jsx for authentication and data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,18 @@ import {
 } from "react-router-dom";
 import './index.css'
 import { router } from './Routes/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './providers/AuthProvider';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+        <AuthProvider>
+        <QueryClientProvider client={queryClient}> 
+
+<RouterProvider router={router} />
+</QueryClientProvider>
+        </AuthProvider>
+
     </StrictMode>,
 )
